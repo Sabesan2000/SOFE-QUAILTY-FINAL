f074384b2fbf4345871ef5e1272d4f5d2c9fe003
Graem Sheppard <graemsheppard@gmail.com>
2021-04-02 19:49:04 -0400

    stash

--- Testing/dynamic-test-data.py
+++ Testing/dynamic-test-data.py
@@ -1,15 +1,20 @@
 from parameterized import parameterized
 from datetime import datetime
 import unittest
+import csv
 import sys
 
 class TestSequence(unittest.TestCase):
-
-    @parameterized.expand([
+    with open('eggs.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        for row in spamreader:
+        print(', '.join(row))
+    arr = [
         ["foo", "a", "a", ],
         ["bar", "a", "b"],
         ["lee", "b", "b"],
-    ])
+    ]
+    @parameterized.expand(arr)
     def test_thing(self, name, a, b):
         unittest.TestCase.assertEqual(self, first=a, second=b, msg=name)
 
